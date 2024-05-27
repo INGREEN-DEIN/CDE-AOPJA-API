d62f339f2e245cda30878649b2a1c5900a08fe94
Tu nombre <i.hinojosa@ingreen.es>
2024-05-27 09:58:51 +0200

    refactor: folder rename from adapter to adapters

--- src/modules/welcome/domain/index.ts
+++ src/modules/welcome/domain/index.ts
@@ -1,4 +1,4 @@
-export * from './entities/welcome.entity';
-export * from './dtos/welcome.dto';
 export * from './datasources/welcome.datasource.interface';
+export * from './dtos/welcome.dto';
+export * from './entities/welcome.entity';
 export * from './repositories/welcome.repository.interface';
